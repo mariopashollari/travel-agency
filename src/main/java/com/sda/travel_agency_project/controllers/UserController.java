e438bb5cb9a1ee5270f3b2e0bf69f25b3492befe
mariopashollari <mariopashollari9@gmail.com>
2025-01-25 13:15:00 +0100

    User Controller

---
+++ src/main/java/com/sda/travel_agency_project/controllers/UserController.java
@@ -0,0 +1,24 @@
+package com.sda.travel_agency_project.controllers;
+
+import com.sda.travel_agency_project.entities.AgencyUser;
+import com.sda.travel_agency_project.services.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/user")
+public class UserController {
+    private final UserService userService;
+
+    @PostMapping("/create")
+    public AgencyUser create(@RequestBody AgencyUser user) {
+        return userService.create(user);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestParam String username, @RequestParam String password) {
+        return userService.login(username, password);
+    }
+}
